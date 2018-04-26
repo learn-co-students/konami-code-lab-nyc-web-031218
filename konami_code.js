6c06f9b4bf050c8b21264ca68cd4736b52da8431
victoria-huang <vctrah@gmail.com>
2018-04-26 19:13:02 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,24 +1,41 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-let index = 0;
-
-function checkCode(event) {
-  const key = parseInt(event.detail || event.which);
-
-  if (key === code[index]) {
-    index++;
-
-    if (index === code.length) {
-      alert("Congrats!!");
-
-      index = 0;
-    }
-  } else {
-    index = 0;
-  }
-}
-
 function init() {
   // Write your JavaScript code inside the init() function
-  document.querySelector('body').addEventListener('keydown', checkCode);
+  let index = 0;
+
+  document.querySelector('body').addEventListener('keydown', function(event) {
+    const key = parseInt(event.detail || event.which);
+
+    if (key === code[index]) {
+      index++;
+
+      if (index === code.length) {
+        alert("Congrats!!");
+
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  });
 }
+
+// 
+// let index = 0;
+//
+// function checkCode(event) {
+//   const key = parseInt(event.detail || event.which);
+//
+//   if (key === code[index]) {
+//     index++;
+//
+//     if (index === code.length) {
+//       alert("Congrats!!");
+//
+//       index = 0;
+//     }
+//   } else {
+//     index = 0;
+//   }
+// }
